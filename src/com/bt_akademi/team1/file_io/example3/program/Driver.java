9766f1f5003655a94d2129c389ccb87d949f6e13
halukalp <haluk.allp@gmail.com>
2022-09-09 00:50:20 +0300

    initial commit

--- src/com/bt_akademi/team1/file_io/example3/program/Driver.java
+++ src/com/bt_akademi/team1/file_io/example3/program/Driver.java
@@ -1,9 +1,41 @@
 package com.bt_akademi.team1.file_io.example3.program;
 
+import com.bt_akademi.team1.utility.Util;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Driver
 {
+    private static final String OKUNACAK_DOSYA = "employees.txt";
+
     public static void main(String[] args)
     {
+        BufferedReader bufferedReader = null;
 
+        try
+        {
+
+        }
+        catch (FileNotFoundException e)
+        {
+            Util.showGeneralException(e);
+        }
+        catch (IOException e)
+        {
+            Util.showGeneralException(e);
+        }
+        finally
+        {
+            try
+            {
+                if(bufferedReader != null) bufferedReader.close();
+            }
+            catch (IOException e)
+            {
+                Util.showGeneralException(e);
+            }
+        }
     }
 }
